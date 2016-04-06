c2d5caec516c9c61db53026db69922f4956a69c6
Will Schaefer III <wschaeferiii@gmail.com>
2016-04-05 22:02:55 -0400

    got the game determining a winner, tie, reset button, game messenger

--- javascript/mainscript.js
+++ javascript/mainscript.js
@@ -1,53 +1,103 @@
-$(document).ready(function() {
-
+var ticTacToe = (function () {
+  //global variables
   var moves = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   var counter = 1;
+  var x = "x";
+  var o = "o";
+  var currentPlayer = x;
+  //the game messager
+  var displayMessage = function(message) {
+    $(".message").html(message);
+  }
+  //switches player after click
+  var switchPlayer = function() {
+    currentPlayer = (currentPlayer === x) ? o : x;
+    displayMessage("Current Player is: " + currentPlayer);
+    console.log(currentPlayer);
 
-  var $gameCells = $('.cell');
-
-  var makeMove = $($gameCells).one('click', function() {
-    var playerMark = (counter % 2 === 0) ? 'o' : 'x';
-    var moveIndex = this.id
-    $("p").eq(moveIndex).text(playerMark);
-    moves[moveIndex] = playerMark;
-    counter++;
-    console.log(counter);
-    testWinner();
-  });
-
-  var testWinner = function() {
+  };
+  //test if move is leagl
+  var isValidMove = function(moveIndex) {
+    if (moves[moveIndex] === 0) {
+      return true;
+    } else {
+      displayMessage("Please play on a blank cell");
+      return false;
+    }
+  };
+  //add player moves to moves array, & mark in box
+  var makeMove = function($cell, moveIndex) {
+      moves[moveIndex] = currentPlayer;
+      $cell.html(currentPlayer);
+      counter++
+  }
+  //test combo
+   var testWinner = function() {
     var winCombinations = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
-      [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ];
-    // var winIndex = -1;
+      [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ],
+      win = 0;
+
     $.each(winCombinations, function(index, winCombination) {
       if (allEqual(winCombination)){
-        // winIndex = index;
-        console.log("we have a winner");
+        switchPlayer();
+        win++
+        displayMessage("game over, " + currentPlayer + "'s won :) ");
         return false;
       }
     });
-    if (counter === 10) {
-      console.log("cats game");
-      return true
-       //cat's game
+    //test draw
+    if (win !== 0) {
+      return reset();
+    } else if (counter === 10) {
+      displayMessage("cats game :/ ")
+      reset();
+      return true;
     } else {
-      return false
+      return false;
     }
-    // if (winIndex !== -1) {
-    //   return winCombinations[winIndex];
-    // } else if (moves === 9) {
-    //   return true; //draw
-    // } else {
-    //   return false;
-    // }
   };
+  //reset button appears after game ends, reloads window
+  var reset = function() {
+    $("#gameboard").off("click");
+    $("button").css("display", "block").on("click", function() {
 
+    });
+  }
+  //game "controller"
+  var playGame = function($cell) {
+    var moveIndex = $cell.attr("id");
+    if (isValidMove(moveIndex)) {
+      makeMove($cell, moveIndex);
+      console.log(moves);
+      switchPlayer();
+    }
+  }
+  //helper function that tests moves-array to winning combos
   var allEqual = function (indexes) {
     return (moves[indexes[0]] === moves[indexes[1]]) &&
       (moves[indexes[0]] === moves[indexes[2]]) &&
       (moves[indexes[0]] !== 0);
   };
+
+  return { playGame: playGame, testWinner: testWinner};
+
+})();
+
+$(document).ready(function() {
+  $("#gameboard").on("click", ".cell", function() {
+    ticTacToe.playGame($(this));
+    ticTacToe.testWinner();
+  });
 });
+
+
+
+
+
+
+
+
+
 
 
 
